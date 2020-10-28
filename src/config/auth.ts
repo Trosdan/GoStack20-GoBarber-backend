f517c8ce5323fac8ea860437c4fe65f08ebaee10
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-10-28 14:51:27 -0300

    Commit Inicial

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '7dc6177498b75dea010b73e9d0e6ff51',
+    expiresIn: '1d',
+  },
+};
---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,43 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs';
+
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+import AppError from '../errors/AppError';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne(user_id);
+
+    if (!user) {
+      throw new AppError('Only authenticated user can change avatar', 401);
+    }
+
+    if (user.avatar) {
+      //Deletar avatar anterios
+
+      const userAvatarFilepath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilepath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilepath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await userRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
