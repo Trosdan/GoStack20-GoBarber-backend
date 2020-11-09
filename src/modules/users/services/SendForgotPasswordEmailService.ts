7ea46aae625f0eb8d0908aab7406d647534bacc6
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-11-09 16:22:08 -0300

    Recuperação de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,9 @@ import { injectable, inject } from 'tsyringe';
 // import AppError from '@shared/errors/AppError';
 import IMailProvidr from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+import AppError from '@shared/errors/AppError';
 
 // import User from '../infra/typeorm/entities/User';
 
@@ -18,9 +21,20 @@ class SendForgotPasswordEmailService {
 
     @inject('MailProvidr')
     private mailProvidr: IMailProvidr,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
   ) {}
 
   public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
     this.mailProvidr.sendMail(
       email,
       'Pedido de recuperação de senha recebido.',
