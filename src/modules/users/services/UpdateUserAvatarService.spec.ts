2fb5f20be5c2fbed5994ea33eff8522ab800b9e3
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-11-10 14:44:27 -0300

    Atualização do perfil

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -22,7 +22,7 @@ describe('UpdateUserAvatar', () => {
   it('should be able to create a new user', async () => {
     const user = await fakeUsersRepository.create({
       name: 'John Doe',
-      email: 'johndoes@example.com',
+      email: 'johndoe@example.com',
       password: '123456',
     });
 
@@ -48,7 +48,7 @@ describe('UpdateUserAvatar', () => {
 
     const user = await fakeUsersRepository.create({
       name: 'John Doe',
-      email: 'johndoes@example.com',
+      email: 'johndoe@example.com',
       password: '123456',
     });
 
