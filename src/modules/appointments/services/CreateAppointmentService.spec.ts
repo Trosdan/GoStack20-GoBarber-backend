ed6433e637f24ea00054f32bc9d27f301384a90a
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-11-10 12:46:20 -0300

    Refatoração dos testes

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -33,7 +33,7 @@ describe('CreateAppointment', () => {
       provider_id: '12312132',
     });
 
-    expect(
+    await expect(
       createAppointment.execute({
         date: appointmentDate,
         provider_id: '12312132',
