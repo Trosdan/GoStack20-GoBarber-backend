dbc2bd18f1f4111d30d945626470cb94e5cdc937
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-11-17 21:45:29 -0300

    Upload de arquivos para o Amazon S3

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -10,7 +10,7 @@ import UsersAvatarController from '../controller/UserAvatarController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 const usersController = new UsersController();
 const usersAvatarController = new UsersAvatarController();
